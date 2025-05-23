d62a92f066854d339f57b6fdfe657449db29819c
Pedro-H-SC <ph.c.stein@gmail.com>
2025-05-23 16:58:11 -0300

    Create atividade2,0.py

---
+++ atividade2,0.py
@@ -0,0 +1,20 @@
+
+nome = str(input('Digite seu nome:'))
+
+idade= int(input('Digite sua idade:'))
+
+ano=int(input('Digite sua serie:'))
+
+
+
+if(idade==0):
+    print('Idade invalida')
+    
+elif(ano>0 and ano<4):
+        print('Seu nome é', nome,', sua idade é' ,idade, ', serie serie é',ano,'º' )
+        
+else:
+    print('Serie invalida')
+        
+        
+        
